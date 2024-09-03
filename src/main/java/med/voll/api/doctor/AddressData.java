2849ab45811b0b86c1a809d179d5e1741157a40b
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-03 13:58:27 -0300

    feat: add record class for storage address Data.

---
+++ src/main/java/med/voll/api/doctor/AddressData.java
@@ -0,0 +1,11 @@
+package med.voll.api.doctor;
+
+public record AddressData (String street,
+                             String neighborhood,
+                             String zipCode,
+                             String city,
+                             String uf,
+                             String complement,
+                             String number){
+
+}
