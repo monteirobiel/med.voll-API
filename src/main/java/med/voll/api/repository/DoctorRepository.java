01de03863957df3f4b9f10d85d78478b83c7f26a
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-05 12:53:39 -0300

    feat: add new interface JpaRepository.

---
+++ src/main/java/med/voll/api/repository/DoctorRepository.java
@@ -0,0 +1,7 @@
+package med.voll.api.repository;
+
+import med.voll.api.doctor.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
