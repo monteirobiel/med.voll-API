9d3b0d4cff2c69f00035d5b931e2c8d27f4fa15f
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-05 15:10:38 -0300

    feat: add @Transactional annotation for Controller.

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -4,6 +4,7 @@ import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorRegisterData;
 import med.voll.api.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class DoctorController {
     private DoctorRepository repository;
 
 
+    @Transactional
     @PostMapping
     public void register(@RequestBody DoctorRegisterData data) {
         repository.save(new Doctor(data));
