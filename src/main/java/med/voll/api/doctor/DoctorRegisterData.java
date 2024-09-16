88c696a77f7f3206d61cbf90175b369206c95573
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-16 10:23:51 -0300

    feat: add new validation.

--- src/main/java/med/voll/api/doctor/DoctorRegisterData.java
+++ src/main/java/med/voll/api/doctor/DoctorRegisterData.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DoctorRegisterData(
 
@@ -13,7 +14,7 @@ public record DoctorRegisterData(
         String email,
         @NotBlank
         String phone,
-        @NotBlank
+        @NotBlank @Pattern(regexp = "\\d{4,6}")
         String crm,
         @NotNull
         Specialty specialty,
