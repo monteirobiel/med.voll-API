47b3e967121fe5f5e3705854c7bb8816acb6fdbf
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-17 14:35:03 -0300

    feat: add put method and attributes to controller.

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import med.voll.api.patients.PatientListingData;
 import med.voll.api.patients.Patient;
 import med.voll.api.patients.PatientRegisterData;
+import med.voll.api.patients.PatientUpdateData;
 import med.voll.api.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -27,5 +28,12 @@ public class PatientController {
     @GetMapping
     public Page<PatientListingData> list(Pageable pageable) {
         return repository.findAllByActiveTrue(pageable).map(PatientListingData::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void update (@RequestBody @Valid PatientUpdateData data) {
+        var doctor = repository.getReferenceById(data.id());
+        doctor.infoUpdate(data);
     }
 }
