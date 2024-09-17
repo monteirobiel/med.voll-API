5c6f016923815dff48945e929735174e5c250acf
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-17 11:33:19 -0300

    feat: add new patient controller with post and get method.

---
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.patients.PatientListingData;
+import med.voll.api.patients.Patient;
+import med.voll.api.patients.PatientRegisterData;
+import med.voll.api.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void register (@RequestBody @Valid PatientRegisterData data) {
+        repository.save(new Patient(data));
+    }
+
+    @GetMapping
+    public Page<PatientListingData> list(Pageable pageable) {
+        return repository.findAllByActiveTrue(pageable).map(PatientListingData::new);
+    }
+}
