e3b17547261308638988a3eafa1a5778564b0239
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-19 14:07:12 -0300

    refactor: replace UserDetails with UserEntity, and change return in method.

--- src/main/java/med/voll/api/repositories/UserRepository.java
+++ src/main/java/med/voll/api/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package med.voll.api.repositories;
 
+import med.voll.api.domain.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByUsername(String username);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
 }
--- src/main/java/med/voll/api/service/AuthService.java
+++ src/main/java/med/voll/api/service/AuthService.java
@@ -1,11 +1,16 @@
 package med.voll.api.service;
 
+import med.voll.api.domain.user.UserEntity;
 import med.voll.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class AuthService implements UserDetailsService {
@@ -15,6 +20,8 @@ public class AuthService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByUsername(username);
+        UserEntity userEntity = repository.findByUsername(username);
+        return new User(userEntity.getUsername(), userEntity.getPassword(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
     }
 }
