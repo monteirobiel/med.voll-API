57be959b86e1292e12cdcb428c4b37da2480067b
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-19 13:40:04 -0300

    feat: add change method from FindByLogin to FindByUsername.

--- src/main/java/med/voll/api/repositories/UserRepository.java
+++ src/main/java/med/voll/api/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByLogin(String username);
+    UserDetails findByUsername(String username);
 }
