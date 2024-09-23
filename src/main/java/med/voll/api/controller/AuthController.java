cc18c41c501737390be6159a1458af1f3bb04e36
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-23 09:15:56 -0300

    feat: add change return and implement new variable.

--- src/main/java/med/voll/api/controller/AuthController.java
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.user.AuthData;
 import med.voll.api.infra.security.TokenService;
+import med.voll.api.infra.security.TokenJwtData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,9 +26,11 @@ public class AuthController {
 
     @PostMapping
     public ResponseEntity login (@RequestBody @Valid AuthData data) {
-        var token = new UsernamePasswordAuthenticationToken(data.username(), data.password());
-        var authentication = manager.authenticate(token);
+        var authToken = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+        var authentication = manager.authenticate(authToken);
 
-        return ResponseEntity.ok(tokenService.generateToken((User) authentication.getPrincipal()));
+        var tokenJwt = tokenService.generateToken((User) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJwtData(tokenJwt));
     }
 }
