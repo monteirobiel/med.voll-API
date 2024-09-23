43dbf8ba3ae21dff53a95ca93e4884614a2dddfa
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-23 12:47:12 -0300

    feat: add filter's logic and token recovery for validation.

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -13,6 +13,17 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = TokenRecovery(request);
+
         filterChain.doFilter(request, response);
+
+    }
+
+    private String TokenRecovery(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            throw new RuntimeException("Token JWT is required on authorization header");
+        }
+        return authHeader.replace("Bearer ", "");
     }
 }
