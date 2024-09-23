a07470427ec318822f0c7f772358675c014cda62
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-23 09:18:20 -0300

    feat: add secrect password

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,12 @@ import java.time.Instant;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret=${JWT_SECRET:12345678}")
+    private String secret;
+
     public String generateToken(User user) {
         try {
-            var algorithm = Algorithm.HMAC256("12345678");
+            var algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(user.getUsername())
