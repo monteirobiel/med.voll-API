0aa50172db12ec0fefc547cd447b2818c91cad71
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-27 15:41:43 -0300

    feat(ScheduleAppointment): add integrity checks for the schedule method.

--- src/main/java/med/voll/api/domain/appointment/ScheduleAppointment.java
+++ src/main/java/med/voll/api/domain/appointment/ScheduleAppointment.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.appointment;
 
+import jakarta.validation.ValidationException;
 import med.voll.api.repositories.AppointmentRepository;
 import med.voll.api.repositories.DoctorRepository;
 import med.voll.api.repositories.PatientRepository;
@@ -18,9 +19,13 @@ public class ScheduleAppointment {
     private DoctorRepository doctorRepository;
 
 
+
     public void schedule(AppointmentRegisterData data) {
-
-
+        if(!patientRepository.existsById(data.idPatient()) ) {
+            throw new ValidationException("Patient not found");
+        }else if(data.idDoctor() != null && !doctorRepository.existsById(data.idDoctor()) ) {
+            throw new ValidationException("Doctor not found");
+        }
 
         var patient = patientRepository.findById(data.idPatient()).isPresent();
         var doctor = doctorRepository.findById(data.idDoctor()).isPresent();
